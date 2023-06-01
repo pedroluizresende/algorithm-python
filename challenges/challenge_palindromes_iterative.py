b86c024993f0d7bc788193b5bb97b2fd90ed752e
Pedro Resende <pedroluizrnunez@gmail.com>
2023-06-01 16:54:46 -0300

    requisito 6 completo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,17 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    end = len(word) - 1
+    # start = 0
+
+    for index in range(len(word)):
+        if index >= end:
+            return True
+        curr_char = word[index]
+        last_char = word[end]
+        if curr_char != last_char:
+            return False
+        end -= 1
+
+
+print(is_palindrome_iterative("i"))
