0b16663c9324d58549992445e37bf0e2baf5ac64
Pedro Resende <pedroluizrnunez@gmail.com>
2023-06-01 15:30:46 -0300

    requisito 2 completo

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message(message="edcba", key=2) == "abc_de"
+    assert encrypt_message(message="edcba", key=3) == "cde_ab"
+    assert encrypt_message(message="abcde", key=5) == "edcba"
+    assert encrypt_message(message="edcba", key=5) == "abcde"
+    assert encrypt_message(message="edcba", key=-5) == "abcde"
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(message=12, key=2)
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message(message='edcba', key='2')
